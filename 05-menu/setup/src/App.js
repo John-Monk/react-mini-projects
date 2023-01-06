77065cea19b081cf83575068bdfa5c73a7198197
John Monk <johntmonk1@gmail.com>
2023-01-06 00:27:37 -0600

    added state, logic, and jsx code to app.js and passed appropriate props to category and menu components

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,19 +3,32 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterCategories = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const filtered = items.filter((item) => item.category === category);
+    setMenuItems(filtered);
+  };
 
   return (
     <main>
       <section className="menu section">
-        <div className="title">
-          <h2>our menu</h2>
-          <div className="underline"></div>
-        </div>
-        <Categories items={items} />
-        <Menu items={items} />
+        <h1 className="title">Menu</h1>
+        <div className="underline"></div>
+        <Categories
+          filterCategories={filterCategories}
+          categories={categories}
+        />
+        <Menu menuItems={menuItems} />
       </section>
     </main>
   );
